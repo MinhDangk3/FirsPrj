e8aaf42ceaa547d04e1d9878fdbbd97d05057bf9
MinhDangk3 <leminhdang1122334455@gmail.com>
2024-08-16 14:25:25 +0700

    first

---
+++ FirsPrj/src/Bai1/Welcome.java
@@ -0,0 +1,10 @@
+package Bai1;
+
+
+
+public class Welcome {
+ public static void main(String[] args) {
+	System.out.println("Hi Le Minh Dang");
+	System.out.println("Welcome to the DHKTPM19BTT");
+}
+}
